530f3f7dc8b86ff3d0614384f2a2c29e0e07d639
Shailesh369545 <159521126+Shailesh369545@users.noreply.github.com>
2025-02-12 00:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const sections = document.querySelectorAll("section");
+        sections.forEach((section) => {
+            section.classList.add("visible");
+        });
+    });
+</script>
